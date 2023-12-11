3765916f259f50dc1a8aa92502d67a7796b7c669
Alisa <alisa.melissa@icloud.com>
2023-12-11 07:27:51 +0200

    Initial Commit

---
+++ ParticleEffect/ParticleEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParticleEffectApp.swift
+//  ParticleEffect
+//
+//  Created by Алиса Третьякова on 11.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct ParticleEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
